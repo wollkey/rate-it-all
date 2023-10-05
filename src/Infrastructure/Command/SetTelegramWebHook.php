1a8e13adbe810eda2da4dcabce2c4b1403f96fbb
Alisher Tashmatov <pirojok167@gmail.com>
2023-10-05 20:11:18 +0300

    Added set webhook command

---
+++ src/Infrastructure/Command/SetTelegramWebHook.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Command;
+
+use App\Infrastructure\Gateway\TelegramApi;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'app:telegram-set-webhook')]
+class SetTelegramWebHook extends Command
+{
+    public function __construct(
+        private readonly TelegramApi $telegram,
+        private readonly string $webhookUrl,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->telegram->setWebHook($this->webhookUrl);
+
+        $output->writeln('<info>Webhook was successfully set!</info>');
+
+        return Command::SUCCESS;
+    }
+}
