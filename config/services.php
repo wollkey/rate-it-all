e445b45e832e3368eaeee31883e32bf7d67ea3f2
Alisher Tashmatov <pirojok167@gmail.com>
2023-10-05 20:08:06 +0300

    Changed configuring services and routes

---
+++ config/services.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use App\Infrastructure\Command\SetTelegramWebHook;
+use Longman\TelegramBot\Telegram;
+
+return function(ContainerConfigurator $container): void {
+    $services = $container->services()
+        ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+    $services->load('App\\', '../src/')
+        ->exclude('../src/{DependencyInjection,Entity,Kernel.php}');
+
+    $services->set(Telegram::class)
+        ->args([
+            '$api_key' => env('TELEGRAM_API_KEY'),
+            '$bot_username' => env('TELEGRAM_BOT_USERNAME'),
+        ]);
+
+    $services->get(SetTelegramWebHook::class)
+        ->arg('$webhookUrl', env('TELEGRAM_WEBHOOK_URL'));
+};
