5c6da1f8dba184c6dbc12c9bd337950b2694c508
Alisher Tashmatov <pirojok167@gmail.com>
2023-10-24 23:03:52 +0300

    Configured deployment

---
+++ config/secrets/prod/prod.list.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'APP_SECRET' => null,
+    'POSTGRES_DB' => null,
+    'POSTGRES_PASSWORD' => null,
+    'POSTGRES_USER' => null,
+    'TELEGRAM_API_KEY' => null,
+    'TELEGRAM_BOT_USERNAME' => null,
+];
