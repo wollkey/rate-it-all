6a43d27e7693889ac57d735cc49837ca79e23fd5
Alisher Tashmatov <pirojok167@gmail.com>
2023-10-24 23:05:26 +0300

    Linter fixes

--- config/secrets/prod/prod.encrypt.public.php
+++ config/secrets/prod/prod.encrypt.public.php
@@ -1,4 +1,6 @@
-<?php // prod.encrypt.public on Tue, 24 Oct 2023 16:59:31 +0000
+<?php
+
+// prod.encrypt.public on Tue, 24 Oct 2023 16:59:31 +0000
 
 return "\xDC\xEF\x93\xD4\xC9P\xD6\xF5\xAA\xE9\xB9Zk\xC68\xAE\xCC\xCC\xA7r\xC3\xE3h\xA0R\x3B\xC4D\x0E\xC8\xD3W";
- // APP_RUNTIME_ENV=prod bin/console secrets:list --reveal
+// APP_RUNTIME_ENV=prod bin/console secrets:list --reveal
