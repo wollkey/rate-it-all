b7e029e1e26aed7eeeb20e6ad0965b2de3fae014
Alisher Tashmatov <pirojok167@gmail.com>
2023-10-25 23:34:08 +0300

    Fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+APP_RUNTIME_ENV=prod php bin/console secrets:decrypt-to-local --force
+cp .env.dev.local .env.local
 composer dump-env prod
 bin/console cache:clear --env=prod --no-debug
 bin/console d:m:m -n --env=prod --no-debug
