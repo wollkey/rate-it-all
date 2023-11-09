2922e3b05822e55642194cd5f4976abe3dfb0885
Alisher Tashmatov <pirojok167@gmail.com>
2023-11-09 18:33:35 +0300

    Configured deployment script for production

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
+composer install
 APP_RUNTIME_ENV=prod php bin/console secrets:decrypt-to-local --force
-cp .env.dev.local .env.local
 composer dump-env prod
 bin/console cache:clear --env=prod --no-debug
 bin/console d:m:m -n --env=prod --no-debug
