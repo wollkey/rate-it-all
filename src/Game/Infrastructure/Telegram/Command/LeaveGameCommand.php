f5b975c3ad9b70d206adf2d4fcc06b5538d762fc
Alisher Tashmatov <pirojok167@gmail.com>
2026-01-13 21:41:14 +0300

    Leave and finish game commands

--- src/Game/Infrastructure/Telegram/Command/LeaveGameCommand.php
+++ src/Game/Infrastructure/Telegram/Command/LeaveGameCommand.php
@@ -9,16 +9,21 @@ use App\Game\Domain\Entity\Player;
 use App\Game\Domain\Exception\ForbiddenActionException;
 use App\Game\Domain\Exception\GameNotFoundException;
 use App\Game\Domain\Repository\PlayerRepository;
+use App\Telegram\AsTelegramCommand;
+use App\Telegram\Domain\Enum\InputType;
 use App\Telegram\TelegramDto;
 use Phptg\BotApi\TelegramBotApi;
+use Phptg\BotApi\Type\InlineKeyboardButton;
+use Phptg\BotApi\Type\InlineKeyboardMarkup;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+#[AsTelegramCommand(self::COMMAND_NAME, inputTypes: [InputType::Text, InputType::Callback])]
 final readonly class LeaveGameCommand
 {
     public const string COMMAND_NAME = '/leave_game';
 
     public function __construct(
-        private TelegramBotApi $telegramApi,
+        private TelegramBotApi $telegram,
         private TranslatorInterface $translator,
         private PlayerRepository $playerRepository,
         private LeaveGameUseCase $leaveGameUseCase,
@@ -28,80 +33,127 @@ final readonly class LeaveGameCommand
     /**
      * @throws \Exception
      */
-    public function execute(TelegramDto $telegramDto): void
+    public function __invoke(TelegramDto $telegramDto): void
     {
         $player = $this->playerRepository->find($telegramDto->user->id);
 
         try {
             ($this->leaveGameUseCase)($player);
         } catch (GameNotFoundException) {
-            $this->sendGameNotFoundMessage($player);
+            $this->sendGameNotFoundMessage($telegramDto, $player);
 
             return;
         } catch (ForbiddenActionException) {
-            $this->sendForbiddenActionMessage($player);
+            $this->sendForbiddenActionMessage($telegramDto, $player);
 
             return;
         }
 
-        $this->telegramApi->sendMessage(
-            $player->getTelegramId(),
-            'See you at another game ![🪇](tg://emoji?id=5368324170671202286)',
-            ['parse_mode' => 'MarkdownV2'],
-        );
-    }
+        if ($telegramDto->isCallback()) {
+            $this->telegram->answerCallbackQuery(
+                callbackQueryId: $telegramDto->callbackQuery->id,
+                showAlert: false,
+            );
 
-    public function supports(TelegramDto $telegramDto): bool
-    {
-        return match (self::COMMAND_NAME) {
-            $this->telegramBot->getMessageCommand($telegramDto->message), $telegramDto->callbackQuery?->data => true,
-            default => false,
-        };
-    }
-
-    /**
-     * @throws \Exception
-     */
-    private function sendGameNotFoundMessage(Player $player): void
-    {
-        $this->telegramApi->sendMessage(
-            $player->getTelegramId(),
-            $this->translator->trans('You are not in any game').PHP_EOL.$this->translator->trans('Create a game or join an existing one'),
-            [
-                'reply_markup' => [
-                    'inline_keyboard' => [[
-                        [
-                            'text' => $this->translator->trans('Create'),
-                            'callback_data' => CreateGameCommand::COMMAND_NAME,
-                        ],
-                        [
-                            'text' => $this->translator->trans('Join'),
-                            'callback_data' => JoinCommand::COMMAND_NAME,
-                        ],
-                    ]],
-                ],
-            ],
-        );
+            $this->telegram->editMessageText(
+                text: 'See you at another game ![🪇](tg://emoji?id=5368324170671202286)',
+                chatId: $telegramDto->message->chat->id,
+                messageId: $telegramDto->message->messageId,
+                replyMarkup: new InlineKeyboardMarkup([
+                    [
+                        new InlineKeyboardButton(
+                            text: '🎮 '.$this->translator->trans('Create new game'),
+                            callbackData: CreateGameCommand::COMMAND_NAME,
+                        ),
+                    ],
+                ]),
+            );
+        } else {
+            $this->telegram->sendMessage(
+                chatId: $telegramDto->message->chat->id,
+                text: 'See you at another game ![🪇](tg://emoji?id=5368324170671202286)',
+                parseMode: 'MarkdownV2',
+                replyMarkup: new InlineKeyboardMarkup([
+                    [
+                        new InlineKeyboardButton(
+                            text: '💀' . $this->translator->trans('Create'),
+                            callbackData: CreateGameCommand::COMMAND_NAME,
+                        ),
+                    ],
+                ]),
+            );
+        }
     }
 
     /**
      * @throws \Exception
      */
-    private function sendForbiddenActionMessage(Player $player): void
+    private function sendGameNotFoundMessage(TelegramDto $telegramDto, Player $player): void
     {
-        $this->telegramApi->sendMessage(
-            $player->getTelegramId(),
-            $this->translator->trans('As master you can only finish the game. Do you really want it?'),
+        $message = $this->translator->trans('You are not in any game').PHP_EOL.$this->translator->trans('Create a game or join an existing one');
+        $keyboard = new InlineKeyboardMarkup([
             [
-                'reply_markup' => [
-                    'inline_keyboard' => [[
-                        [
-                            'text' => $this->translator->trans('Finish the game'),
-                            'callback_data' => FinishGameCommand::COMMAND_NAME,
-                        ],
-                    ]],
-                ],
+                new InlineKeyboardButton(
+                    text: $this->translator->trans('Create'),
+                    callbackData: CreateGameCommand::COMMAND_NAME,
+                ),
             ],
-        );
+        ]);
+
+        if ($telegramDto->isCallback()) {
+            $this->telegram->answerCallbackQuery(
+                callbackQueryId: $telegramDto->callbackQuery->id,
+                showAlert: false,
+            );
+
+            $this->telegram->editMessageText(
+                text: $message,
+                chatId: $telegramDto->message->chat->id,
+                messageId: $telegramDto->message->messageId,
+                replyMarkup: $keyboard,
+            );
+        } else {
+            $this->telegram->sendMessage(
+                chatId: $telegramDto->message->chat->id,
+                text: $message,
+                replyMarkup: $keyboard,
+            );
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function sendForbiddenActionMessage(TelegramDto $telegramDto, Player $player): void
+    {
+        $message = $this->translator->trans('As master you can only finish the game. Do you really want it?');
+        $keyboard = new InlineKeyboardMarkup([
+            [
+                new InlineKeyboardButton(
+                    text: '💀' . $this->translator->trans('Finish the game'),
+                    callbackData: FinishGameCommand::COMMAND_NAME,
+                ),
+            ],
+        ]);
+
+        if ($telegramDto->isCallback()) {
+            $this->telegram->answerCallbackQuery(
+                callbackQueryId: $telegramDto->callbackQuery->id,
+                showAlert: false,
+            );
+
+            $this->telegram->editMessageText(
+                text: $message,
+                chatId: $telegramDto->message->chat->id,
+                messageId: $telegramDto->message->messageId,
+                replyMarkup: $keyboard,
+            );
+        } else {
+            $this->telegram->sendMessage(
+                chatId: $telegramDto->message->chat->id,
+                text: $message,
+                replyMarkup: $keyboard,
+            );
+        }
     }
 }
