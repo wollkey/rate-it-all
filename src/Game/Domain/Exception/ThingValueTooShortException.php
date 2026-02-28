a25099030bc8bdb35920bdfb3498d9dd41848c5d
Alisher Tashmatov <pirojok167@gmail.com>
2026-02-28 22:32:26 +0300

    Linter fixes

--- src/Game/Domain/Exception/ThingValueTooShortException.php
+++ src/Game/Domain/Exception/ThingValueTooShortException.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types=1);
+
 namespace App\Game\Domain\Exception;
 
-final class ThingValueTooShortException  extends GameException
+final class ThingValueTooShortException extends GameException
 {
 }
