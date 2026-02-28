3bc4e899f6a1489de214bb2a98894e9cc9531115
Alisher Tashmatov <pirojok167@gmail.com>
2026-02-28 20:38:28 +0300

    Fixed dispatching aggregate root events

--- src/Game/Infrastructure/Orm/DomainEventDispatcher.php
+++ src/Game/Infrastructure/Orm/DomainEventDispatcher.php
@@ -6,52 +6,30 @@ namespace App\Game\Infrastructure\Orm;
 
 use App\Game\Domain\Aggregate\AggregateRoot;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
-use Doctrine\ORM\Event\PostPersistEventArgs;
-use Doctrine\ORM\Event\PostUpdateEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
 use Doctrine\ORM\Events;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
-#[AsDoctrineListener(event: Events::postPersist)]
-#[AsDoctrineListener(event: Events::postUpdate)]
 #[AsDoctrineListener(event: Events::postFlush)]
-final class DomainEventDispatcher
+final readonly class DomainEventDispatcher
 {
-    /**
-     * @var list<AggregateRoot>
-     */
-    private array $entities = [];
-
     public function __construct(
-        private readonly EventDispatcherInterface $eventDispatcher,
+        private EventDispatcherInterface $eventDispatcher,
     ) {
     }
 
-    public function postPersist(PostPersistEventArgs $args): void
+    public function postFlush(PostFlushEventArgs $args): void
     {
-        $this->collect($args->getObject());
-    }
+        $uow = $args->getObjectManager()->getUnitOfWork();
 
-    public function postUpdate(PostUpdateEventArgs $args): void
-    {
-        $this->collect($args->getObject());
-    }
-
-    public function postFlush(): void
-    {
-        $entities = $this->entities;
-        $this->entities = [];
-
-        foreach ($entities as $entity) {
-            foreach ($entity->pullEvents() as $event) {
-                $this->eventDispatcher->dispatch($event);
+        foreach ($uow->getIdentityMap() as $entities) {
+            foreach ($entities as $entity) {
+                if ($entity instanceof AggregateRoot) {
+                    foreach ($entity->pullEvents() as $event) {
+                        $this->eventDispatcher->dispatch($event);
+                    }
+                }
             }
-        }
-    }
-
-    private function collect(object $entity): void
-    {
-        if ($entity instanceof AggregateRoot) {
-            $this->entities[] = $entity;
         }
     }
 }
