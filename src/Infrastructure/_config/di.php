d8a151923d68aaf7f06d595b4d43c71532f64e00
Alisher Tashmatov <pirojok167@gmail.com>
2026-03-12 22:10:48 +0300

    Removed SentryTestController

--- src/Infrastructure/_config/di.php
+++ src/Infrastructure/_config/di.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use App\Infrastructure\Application\LocaleSubscriber;
-use App\Infrastructure\Http\SentryTestController;
 use Monolog\Level;
 use Sentry\SentryBundle\Monolog\LogsHandler;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -16,8 +15,6 @@ return static function (ContainerConfigurator $container): void {
 
     $services->set(LocaleSubscriber::class)
         ->arg('$defaultLocale', '%kernel.default_locale%');
-
-    $services->set(SentryTestController::class);
 
     if ($container->env() === 'prod') {
         $services->set(LogsHandler::class)
